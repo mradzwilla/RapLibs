f44d60940618025889a7da8103b2882e5d2b60e2
Michael <mradzwilla@gmail.com>
2016-04-25 03:46:18 -0400

    troubleshooting heroku

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
 
 module RapLibs
   class Application < Rails::Application
@@ -23,5 +22,8 @@ module RapLibs
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.initialize_on_precompile = false
+
   end
 end
